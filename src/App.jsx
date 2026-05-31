9add7807182a46c71695adf1f0a62ece9259eb7f
MohamadAbouSamra <mhamadsamra34@gmail.com>
2026-05-31 12:12:35 -0700

    Create NavBar Components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      
+    </>
+  );
+}
+
+export default App;
